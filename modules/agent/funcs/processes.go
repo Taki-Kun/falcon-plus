2ec4c6f3d7785db8c3abdf321fbf030943d83ef2
issenn <issenn@hellotalk.com>
2019-01-28 18:31:10 +0800

    Initial commit

--- modules/agent/funcs/processes.go
+++ modules/agent/funcs/processes.go
@@ -17,8 +17,8 @@ package funcs
 import (
 	"github.com/Taki-Kun/falcon-plus/common/model"
 	"github.com/Taki-Kun/falcon-plus/modules/agent/g"
-	"github.com/toolkits/nux"
 	"github.com/Taki-Kun/process"
+	"github.com/toolkits/nux"
 	"sync"
 )
 
@@ -100,26 +100,91 @@ func ProcessMetrics() (L []*model.MetricValue) {
 		if !ProcessPrepared(tags) {
 			continue
 		}
+		var processCpuPercent float64
 		var processMemRss uint64
 		var processMemVms uint64
 		var processMemSwap uint64
 		var processMemData uint64
 		var processMemStack uint64
 		var processMemLocked uint64
+		var processNumThreads int32
+		var processNumFds int32
+		var processNumCtxSwitchesVoluntary int64
+		var processNumCtxSwitchesInvoluntary int64
+		var processIOCountersReadCount uint64
+		var processIOCountersWriteCount uint64
+		var processIOCountersReadBytes uint64
+		var processIOCountersWriteBytes uint64
+		var processNetIOCountersBytesSent uint64
+		var processNetIOCountersBytesRecv uint64
+		var processNetIOCountersPacketsSent uint64
+		var processNetIOCountersPacketsRecv uint64
+		var processNetIOCountersErrin uint64
+		var processNetIOCountersErrout uint64
+		var processNetIOCountersDropin uint64
+		var processNetIOCountersDropout uint64
+		var processNetIOCountersFifoin uint64
+		var processNetIOCountersFifoout uint64
+		var processNumNetConnections int
 		for _, processStat := range processesStat {
+			processCpuPercent = processCpuPercent + processStat.CpuInfo.Percent
 			processMemRss = processMemRss + processStat.MemInfo.RSS
 			processMemVms = processMemVms + processStat.MemInfo.VMS
 			processMemSwap = processMemSwap + processStat.MemInfo.Swap
 			processMemData = processMemData + processStat.MemInfo.Data
 			processMemStack = processMemStack + processStat.MemInfo.Stack
 			processMemLocked = processMemLocked + processStat.MemInfo.Locked
+			processNumThreads = processNumThreads + processStat.NumThreads
+			processNumFds = processNumFds + processStat.Fd
+			processNumCtxSwitchesVoluntary = processNumCtxSwitchesVoluntary + processStat.NumCtxSwitches.Voluntary
+			processNumCtxSwitchesInvoluntary = processNumCtxSwitchesInvoluntary + processStat.NumCtxSwitches.Involuntary
+			processIOCountersReadCount = processIOCountersReadCount + processStat.IOCounters.ReadCount
+			processIOCountersWriteCount = processIOCountersWriteCount + processStat.IOCounters.WriteCount
+			processIOCountersReadBytes = processIOCountersReadBytes + processStat.IOCounters.ReadBytes
+			processIOCountersWriteBytes = processIOCountersWriteBytes + processStat.IOCounters.WriteBytes
+			processNumNetConnections = processNumNetConnections + len(processStat.NetConnection)
+			for _, processNetIOCountersStat := range processStat.NetIOCounters {
+				if processNetIOCountersStat.Name != "eth0" {
+					continue
+				}
+				processNetIOCountersBytesSent = processNetIOCountersBytesSent + processNetIOCountersStat.BytesSent
+				processNetIOCountersBytesRecv = processNetIOCountersBytesRecv + processNetIOCountersStat.BytesRecv
+				processNetIOCountersPacketsSent = processNetIOCountersPacketsSent + processNetIOCountersStat.PacketsSent
+				processNetIOCountersPacketsRecv = processNetIOCountersPacketsRecv + processNetIOCountersStat.PacketsRecv
+				processNetIOCountersErrin = processNetIOCountersErrin + processNetIOCountersStat.Errin
+				processNetIOCountersErrout = processNetIOCountersErrout + processNetIOCountersStat.Errout
+				processNetIOCountersDropin = processNetIOCountersDropin + processNetIOCountersStat.Dropin
+				processNetIOCountersDropout = processNetIOCountersDropout + processNetIOCountersStat.Dropout
+				processNetIOCountersFifoin = processNetIOCountersFifoin + processNetIOCountersStat.Fifoin
+				processNetIOCountersFifoout = processNetIOCountersFifoout + processNetIOCountersStat.Fifoout
+			}
 		}
+		L = append(L, GaugeValue(g.PROCESS_CPU_BUSY_PERCENT, processCpuPercent, tags))
 		L = append(L, GaugeValue(g.PROCESS_MEM_RSS, processMemRss, tags))
 		L = append(L, GaugeValue(g.PROCESS_MEM_VMS, processMemVms, tags))
 		L = append(L, GaugeValue(g.PROCESS_MEM_SWAP, processMemSwap, tags))
 		L = append(L, GaugeValue(g.PROCESS_MEM_DATA, processMemData, tags))
 		L = append(L, GaugeValue(g.PROCESS_MEM_STACK, processMemStack, tags))
 		L = append(L, GaugeValue(g.PROCESS_MEM_LOCKED, processMemLocked, tags))
+		L = append(L, GaugeValue(g.PROCESS_THREADS_NUMBER, processNumThreads, tags))
+		L = append(L, GaugeValue(g.PROCESS_FD_NUMBER, processNumFds, tags))
+		L = append(L, GaugeValue(g.PROCESS_CTXSWITCHESVOLUNTARY, processNumCtxSwitchesVoluntary, tags))
+		L = append(L, GaugeValue(g.PROCESS_CTXSWITCHESINVOLUNTARY, processNumCtxSwitchesInvoluntary, tags))
+		L = append(L, GaugeValue(g.PROCESS_IOCOUNTERS_READCOUNT, processIOCountersReadCount, tags))
+		L = append(L, GaugeValue(g.PROCESS_IOCOUNTERS_WRITECOUNT, processIOCountersWriteCount, tags))
+		L = append(L, GaugeValue(g.PROCESS_IOCOUNTERS_READBYTES, processIOCountersReadBytes, tags))
+		L = append(L, GaugeValue(g.PROCESS_IOCOUNTERS_WRITEBYTES, processIOCountersWriteBytes, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_BYTESSENT, processNetIOCountersBytesSent, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_BYTESRECV, processNetIOCountersBytesRecv, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_PACKETSSENT, processNetIOCountersPacketsSent, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_PACKETSRECV, processNetIOCountersPacketsRecv, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_ERRIN, processNetIOCountersErrin, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_ERROUT, processNetIOCountersErrout, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_DROPIN, processNetIOCountersDropin, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_DROPOUT, processNetIOCountersDropout, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_FIFOIN, processNetIOCountersFifoin, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_FIFOOUT, processNetIOCountersFifoout, tags))
+		L = append(L, GaugeValue(g.PROCESS_NETIOCOUNTERS_NUMBER, processNumNetConnections, tags))
 	}
 
 	return
